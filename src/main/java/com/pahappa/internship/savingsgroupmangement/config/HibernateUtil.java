c82dbe01fc6e50ed63a1675edecf269bfdb5113d
sekibaalamark <markmajje256@gmail.com>
2026-07-20 16:11:43 +0300

    Wrote helper classes for hibernate usage

---
+++ src/main/java/com/pahappa/internship/savingsgroupmangement/config/HibernateUtil.java
@@ -0,0 +1,25 @@
+package com.pahappa.internship.savingsgroupmangement.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Database Initial SessionFactory configuration failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
